bf43ac5232c67d3150d3eb3f63871dd74c91a200
kadiraswapna123 <kadiraswapna@gmail.com>
2022-07-26 16:37:57 +0530

    SinglePlayer

---
+++ PositionOfTheDie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class PositionOfTheDie
+    {
+        static int maxGameByWinner(int N)
+        {
+            int[] dp = new int[N];
+
+            // for 0 games, 1 player is needed
+            // for 1 game, 2 players are required
+            dp[0] = 1;
+            dp[1] = 2;
+
+            int i = 2;
+
+            do
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            } while (dp[i++] <= N);
+
+            return (i - 2);
+        }
+
+        // Driver code
+        public static void Main()
+        {
+            int N = 10;
+            Console.Write(maxGameByWinner(N));
+        }
+    }
+}
+
---
+++ RollsTheDie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class RollsTheDie
+    {
+        Random random = new Random();
+        public int RollStandardDie()
+        {
+            int dice1 = random.Next(1, 6);
+            int dice2 = random.Next(1, 6);
+            return dice1 + dice2;
+        } 
+        int[] dice1 = new int[3];
+        int[] dice2 = new int[3];           
+        Dictionary<int, int> dice3 = new Dictionary<int, int>();    
+    }
+    Console.WriteLine(RollStandardDie);
+}
